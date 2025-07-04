dacac7502889436edb9dd238013013311a8acf53
Martí <fontyfontfreda@gmail.com>
2025-07-04 14:32:33 +0200

    Redireccio port

--- server.js
+++ server.js
@@ -8,6 +8,20 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+//afegit guillem
+const https = require('https');
+const fs = require('fs');
+
+https.createServer({
+    cert: fs.readFileSync('ssl/certificate.crt'),
+    key: fs.readFileSync('ssl/certificate.key')
+},app.get("/", (req, res) => {
+    res.writeHead(200);
+    res.end('Hello from Node!\n');
+})).listen(3443);
+
+//fi afegit Gyuillem
+
 // Permet fins a 50 MB per JSON i formularis
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
@@ -24,7 +38,7 @@ connectDB()
   .then(() => {
     console.log(`🚀 Connexió a la base de dades establerta amb èxit!`);
     app.listen(PORT, () => {
-      console.log(`🚀 Backend executant-se a http://localhost:${PORT}`);
+      console.log(`🚀 Backend executant-se a https://localhost:${PORT}`);
     });
   })
   .catch((err) => {
