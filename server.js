f4f0a11762854d57c346cebb62acd7193b94dfd4
Marti.Fontfreda <Marti.Fontfreda@olot.cat>
2026-04-20 07:56:47 +0200

    Afegida condicio per si estem al servidor. ATENCIÓ, ACTUALITZAR .env

--- server.js
+++ server.js
@@ -1,38 +1,43 @@
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
-const routes = require('./routes/routes');
-const connectDB = require('./models/db');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const routes = require("./routes/routes");
+const connectDB = require("./models/db");
+const isServer = process.env.IS_SERVER === 'true';
+
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+if (isServer) {
+  const https = require("https");
+  const fs = require("fs");
 
-//afegit guillem
-const https = require('https');
-const fs = require('fs');
-
-https.createServer({
-    cert: fs.readFileSync('ssl/certificate.crt'),
-    key: fs.readFileSync('ssl/certificate.key')
-},app.get("/", (req, res) => {
-    res.writeHead(200);
-    res.end('Hello from Node!\n');
-})).listen(3443);
-
-//fi afegit Gyuillem
+  https
+    .createServer(
+      {
+        cert: fs.readFileSync("ssl/certificate.crt"),
+        key: fs.readFileSync("ssl/certificate.key"),
+      },
+      app.get("/", (req, res) => {
+        res.writeHead(200);
+        res.end("Hello from Node!\n");
+      }),
+    )
+    .listen(3443);
+}
 
 // Permet fins a 50 MB per JSON i formularis
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 app.use(cors());
 
 // Exposa públicament la carpeta d’imatges
-app.use('/imatges', express.static(path.join(process.env.IMATGE_RUTA)));
+app.use("/imatges", express.static(path.join(process.env.IMATGE_RUTA)));
 
 // Rutes de l'API
-app.use('/api', routes);
+app.use("/api", routes);
 
 connectDB()
   .then(() => {
@@ -42,6 +47,6 @@ connectDB()
     });
   })
   .catch((err) => {
-    console.error('❌ Error al connectar-se a la base de dades:', err);
+    console.error("❌ Error al connectar-se a la base de dades:", err);
     process.exit(1);
   });
